b40bd57b92916d0411f955c2245ded92aabe3e11
manand10 <manand10@LH7U05CG6422Q5G.uhc.com>
2019-03-11 09:49:42 +0530

    QuesCommit

---
+++ AssignmetQues4/src/setandlist/methodclass.java
@@ -0,0 +1,32 @@
+package setandlist;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class methodclass {
+	
+	
+	public List<String> ListMethod()
+	{
+		List<String> ar2=new ArrayList<String>();
+		ar2.add("A");
+		ar2.add("B");
+		return ar2;
+	}
+
+	public Set<Object> SetMethod()
+	{
+		Set<Object> s=new HashSet<Object>();
+		s.add("B");
+		s.add("C");
+		s.add("H");
+		s.add("B");
+		s.add("C");
+		return s;
+		
+	}
+	
+}
